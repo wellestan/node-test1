54ac210411f1f2a12f2e867631bc9a8610259043
Welles Tan <twhe@163.com>
2026-06-09 00:50:59 +0800

    update node verstion to 24

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { resolve } from 'path';
 
 export default defineConfig({
   build: {
-    target: 'node26',
+    target: 'node24',
     outDir: 'dist',
     rollupOptions: {
       input: resolve(__dirname, 'index.js'),
